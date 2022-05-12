91770ab902aaa7aad12544693fcf9e5c978eafb3
royce79-creator <connorsboyce07@gmail.com>
2022-05-11 21:36:33 -0700

    Have the back_end working, need to gover over uncommented code in the settings.py, almost have back end up and running, had issues with whitespace on .env, good learning lesson

--- back_end/party_up_admin/urls.py
+++ back_end/party_up_admin/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     # Issues with line 24 -> Recursion occurs when this line is inputted(endless loop).
     path('/', include('party_up_api.urls')),
     path('api-auth', include('rest_framework.urls')),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
--- back_end/party_up_api/urls.py
+++ back_end/party_up_api/urls.py
@@ -4,7 +4,6 @@ from rest_framework_simplejwt import views as jwt_views
 from .views import Profile_Detail, Profile_List
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('/', Profile_List.as_view(), name = 'profile_list'),
-    path('/', Profile_Detail.as_view(), name = 'profile_detail'),
+    path('profile', Profile_List.as_view(), name = 'profile_list'),
+    path('profile_detail', Profile_Detail.as_view(), name = 'profile_detail'),
 ]
