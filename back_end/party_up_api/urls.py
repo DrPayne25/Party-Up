51c111df51e4ee0c256f9364f6bedd09e3218b94
royce79-creator <connorsboyce07@gmail.com>
2022-05-11 19:33:49 -0700

    need the migrations to be fixed

--- back_end/party_up_api/urls.py
+++ back_end/party_up_api/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
+from .views import Profile_Detail, Profile_List
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('party_up_api.urls')),
+    # path('admin/', admin.site.urls),
+    path('/', Profile_List.as_view(), name = 'profile_list'),
+    path('/', Profile_Detail.as_view(), name = 'profile_detail'),
 ]
