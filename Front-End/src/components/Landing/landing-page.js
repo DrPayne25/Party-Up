cc3f393b2effa3bd726cfeb77d5408a6058cb9aa
micgreene <mgreene11111@yahoo.com>
2022-05-16 21:01:19 -0700

    trying to use user token gained from Auth0 login to begin an automatic API call when logging in for the first time ever, had to fix warnings for netlify to deploy properly

--- Front-End/src/components/Landing/landing-page.js
+++ Front-End/src/components/Landing/landing-page.js
@@ -7,6 +7,10 @@ const LandingPage = () => {
     const { user, isAuthenticated, isLoading } = useAuth0();
     const { first_name, last_name, username, email, about_me } = user;
 
+    if (isLoading) {
+        return <div>Loading ...</div>;
+    }
+    
     if (!isAuthenticated){
         return (
             <div>
@@ -21,7 +25,11 @@ const LandingPage = () => {
             <div>
                 <Header />
                 <h1>Partying On a Tuesday, Eh Lou?</h1>
+                <p>{first_name}</p>
+                <p>{last_name}</p>
+                <p>{username}</p>
                 <p>{email}</p>
+                <p>{about_me}</p>
                 
             </div>
         )
